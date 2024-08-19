ef659edd69b89f0bc1bf551ea3bf3c07c251fbd7
Niclas Wuestenbecker <niclaswue@gmail.com>
2024-08-19 09:32:13 +0200

    modular scikit-learn model

--- models/scikit_learn_model.py
+++ models/scikit_learn_model.py
@@ -3,16 +3,19 @@ from sklearn.base import BaseEstimator
 from models.base_model import BaseModel
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error, r2_score
+from sklearn.preprocessing import StandardScaler
+from sklearn.pipeline import make_pipeline
 
 
 class ScikitLearnModel(BaseModel):
 
-    def __init__(self, model_class, model_params=None, test_size=0.2):
+    def __init__(self, model_class, model_params=None, test_size=0.2, name: str = None):
         if not issubclass(model_class, BaseEstimator):
             raise ValueError("model_class must be a scikit-learn estimator")
 
-        name = model_class().__repr__()
-        name = name.split("(")[0] if "(" in name else name
+        if not name:
+            name = model_class().__repr__()
+            name = name.split("(")[0] if "(" in name else name
         super().__init__(name)
         self.model_class = model_class
         self.model_params = model_params or {}
@@ -22,21 +25,26 @@ class ScikitLearnModel(BaseModel):
         self.target_column = "tow"
         self.metrics = {}
 
-    def _preprocess_df(self, df: pd.DataFrame):
+    def _preprocess_inputs(self, df: pd.DataFrame):
         # drop columnms not useful for prediction
-        drop_cols = ["flight_id", "callsign", "name_adep", "name_ades"]
-        df = df.drop(columns=drop_cols + [self.target_column])
+        drop_cols = ["flight_id", "name_adep", "name_ades"] + [self.target_column]
+        X = df.drop(columns=drop_cols, errors="ignore")
+        y = df[self.target_column] if self.target_column in df.columns else None
 
         # convert objects to categorical
-        objs = df.select_dtypes(["object"])
-        df[objs.columns] = objs.apply(lambda x: x.astype("category").cat.codes)
+        objs = X.select_dtypes(["object"])
+        X[objs.columns] = objs.apply(lambda x: x.astype("category").cat.codes)
 
-        return df
+        return X, y
+
+    def _postprocess_output(self, input_df, output):
+        if self.model_params.get("round_output"):
+            output = output.round()
+        return output
 
     def train(self, training_df: pd.DataFrame):
         # Separate features and target
-        X = self._preprocess_df(training_df)
-        y = training_df[self.target_column]
+        X, y = self._preprocess_inputs(training_df)
 
         # Store feature column names
         self.feature_columns = X.columns.tolist()
@@ -47,15 +55,16 @@ class ScikitLearnModel(BaseModel):
         )
 
         # Initialize and train the model
-        self.model = self.model_class(**self.model_params)
+        pipeline = [StandardScaler(), self.model_class(**self.model_params)]
+        self.model = make_pipeline(*pipeline)
         print("model.fit")
         self.model.fit(X_train, y_train)
         print("Done")
 
-        # Calculate metrics
         y_pred_train = self.model.predict(X_train)
         y_pred_test = self.model.predict(X_test)
 
+        # Calculate metrics
         self.metrics = {
             "train_mse": mean_squared_error(y_train, y_pred_train),
             "test_mse": mean_squared_error(y_test, y_pred_test),
@@ -66,8 +75,10 @@ class ScikitLearnModel(BaseModel):
     def predict(self, input_df: pd.DataFrame):
         if self.model is None:
             raise ValueError("Model has not been trained yet")
-        X = self._preprocess_df(input_df)
-        return pd.Series(self.model.predict(X))
+        X, _ = self._preprocess_inputs(input_df)
+        y = self.model.predict(X)
+        y = self._postprocess_output(X, y)
+        return pd.Series(y)
 
     def info(self):
         if self.model is None:
