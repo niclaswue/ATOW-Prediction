d8e2cb3fdd561dc66119ea12311471767997d80b
Niclas Wuestenbecker <niclaswue@gmail.com>
2024-09-02 06:56:27 +0200

    add two more derived features

--- preprocessing/derived_features.py
+++ preprocessing/derived_features.py
@@ -8,5 +8,7 @@ def add_derived_features(dataset):
     dataset.df["month"] = pd.to_datetime(dataset.df.date).dt.month
     dataset.df["year"] = pd.to_datetime(dataset.df.date).dt.year
     dataset.df["day_of_week"] = pd.to_datetime(dataset.df.date).dt.day_of_week
+    dataset.df["quarter"] = pd.to_datetime(dataset.df.date).dt.quarter
+    dataset.df["is_week_day"] = dataset.df["day_of_week"] < 5
     print("Done.")
     return dataset
