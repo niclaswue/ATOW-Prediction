bf5e64e96e8354dc8ebe90741aeb3606ba1fb617
Niclas Wuestenbecker <niclaswue@gmail.com>
2024-09-04 12:41:36 +0200

    expose verbosity

--- models/autogluon_model.py
+++ models/autogluon_model.py
@@ -6,17 +6,22 @@ from models.base_model import BaseModel
 class AutogluonModel(BaseModel):
 
     def __init__(
-        self, time_limit=5 * 60, preset="high_quality", name: str = "autogluon"
+        self,
+        time_limit=5 * 60,
+        preset="high_quality",
+        verbosity: int = 2,
+        name: str = "autogluon",
     ):
         super().__init__(name)
         self.time_limit = time_limit
         self.presets = [preset]
+        self.verbosity = verbosity
 
     def train(self, training_df: pd.DataFrame):
         train_data = TabularDataset(training_df)
         predictor = TabularPredictor(
             label="tow",
-            verbosity=2,
+            verbosity=self.verbosity,
             # log_to_file=True,
             # log_file_path="/home/niclas/ATOW-Prediction/autogluon.log",
         ).fit(train_data, time_limit=self.time_limit, presets=self.presets)
