2bf3b900bf483ee3bb65dae232fda95933764376
Niclas Wüstenbecker <niclas.wuestenbecker@dlr.de>
2024-09-23 17:59:43 +0200

    updated aircraft info and lut for airline

--- preprocessing/clean_dataset.py
+++ preprocessing/clean_dataset.py
@@ -4,7 +4,6 @@ from preprocessing.base_preprocessor import BasePreprocessor
 
 
 class CleanDatasetPreprocessor(BasePreprocessor):
-
     def process(self, dataset: Dataset) -> Dataset:
         # TODO: Remove duplicates in data etc.
         datetime_cols = ["date", "actual_offblock_time", "arrival_time", "valid"]
@@ -12,4 +11,7 @@ class CleanDatasetPreprocessor(BasePreprocessor):
             if c not in dataset.df.columns:
                 continue
             dataset.df[c] = pd.to_datetime(dataset.df[c]).astype(int)
+
+        # never seen in submission, only train data
+        dataset.df = dataset.df[dataset.df["aircraft_type"] != "C56X"]
         return dataset
