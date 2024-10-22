b5d4d5ba1984f8b34bfc767351ff4856379b793b
Niclas Wüstenbecker <niclas.wuestenbecker@dlr.de>
2024-10-22 08:02:57 +0200

    clean dataset

--- preprocessing/clean_dataset.py
+++ preprocessing/clean_dataset.py
@@ -14,4 +14,61 @@ class CleanDatasetPreprocessor(BasePreprocessor):
 
         # never seen in submission, only train data
         dataset.df = dataset.df[dataset.df["aircraft_type"] != "C56X"]
+        dataset.df = dataset.df[dataset.df["aircraft_type"] != "A310"]
+
+        # drop columns not found to be important using feature importance analysis
+        drop_cols = [
+            "ades_mslp",
+            "stats_PAS_PAS_TRF_UNK_x",
+            "Quantity_Kerosene-type Jet Fuel - Consumption by manufacturing, construction and non-fuel mining industry_x",
+            "stats_PAS_PAS_TRF_UNK_y",
+            "ades_p01i",
+            "stats_MOVE_CACM_LIC_NEU_y",
+            "ades_skyl4",
+            "stats_T_FRM_LD_NLD_UNK_y",
+            "stats_PAS_PAS_TRS_UNK_x",
+            "stats_PAS_PAS_TRF_LIC_EU_y",
+            "stats_MOVE_CACM_UNK_y",
+            "stats_PAS_PAS_TRS_UNK_y",
+            "stats_PAS_PAS_CRD_LIC_EU_y",
+            "stats_PAS_PAS_TRF_LIC_NEU_y",
+            "stats_PAS_PAS_CRD_LIC_EU_x",
+            "ades_continent",
+            "stats_MOVE_ACM_LIC_NEU_y",
+            "ades_skyc4",
+            "stats_T_FRM_LD_NLD_LIC_EU_y",
+            "stats_T_FRM_LD_NLD_UNK_x",
+            "stats_MOVE_CACM_UNK_x",
+            "Quantity_Kerosene-type Jet Fuel - Consumption by other_y",
+            "stats_T_FRM_LD_NLD_LIC_NEU_y",
+            "stats_T_FRM_LD_NLD_LIC_EU_x",
+            "stats_MOVE_ACM_UNK_y",
+            "stats_MOVE_CACM_LIC_NEU_x",
+            "stats_MOVE_ACM_LIC_EU_x",
+            "stats_PAS_PAS_TRF_LIC_NEU_x",
+            "ades_skyc3",
+            "stats_MOVE_ACM_UNK_x",
+            "stats_MOVE_CACM_LIC_EU_y",
+            "stats_MOVE_ACM_LIC_NEU_x",
+            "stats_PAS_PAS_CRD_UNK_y",
+            "Quantity_Kerosene-type Jet Fuel - Final consumption_y",
+            "stats_PAS_PAS_CRD_LIC_NEU_y",
+            "Quantity_Kerosene-type Jet Fuel - Consumption by other_x",
+            "stats_PAS_PAS_TRF_LIC_EU_x",
+            "stats_PAS_PAS_CRD_UNK_x",
+            "stats_PAS_PAS_CRD_LIC_NEU_x",
+            "Quantity_Kerosene-type Jet Fuel - Exports_y",
+            "Quantity_Kerosene-type Jet Fuel - Consumption by domestic aviation_y",
+            "stats_PAS_PAS_TRS_LIC_NEU_y",
+            "stats_PAS_PAS_TRS_LIC_NEU_x",
+            "stats_PAS_PAS_TRS_LIC_EU_y",
+            "stats_T_FRM_LD_NLD_LIC_NEU_x",
+            "stats_MOVE_ACM_LIC_EU_y",
+            "stats_MOVE_CACM_LIC_EU_x",
+            "stats_PAS_PAS_TRS_LIC_EU_x",
+            "adep_continent",
+        ]
+
+        dataset.df.drop(columns=drop_cols, inplace=True)
+
         return dataset
