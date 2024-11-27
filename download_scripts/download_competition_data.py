16db6d99b4889f5c7b48a7c371cba3efc55af9ec
Niclas Wüstenbecker <niclas.wuestenbecker@dlr.de>
2024-11-27 12:10:20 +0100

    fix download script

--- download_scripts/download_competition_data.py
+++ download_scripts/download_competition_data.py
@@ -7,9 +7,9 @@ data_dir.mkdir(exist_ok=True)
 
 s3 = S3Client()
 objects = [obj for obj in s3.s3client.list_objects("competition-data", recursive=True)]
-objects = [obj for obj in objects if not (Path("data") / obj.object_name).exists()]
+objects = [obj for obj in objects if not (data_dir / obj.object_name).exists()]
 objects = reversed(objects)  # to get csv files first
 
 for obj in tqdm(objects):
     print(f"Downloading {obj.object_name}")
-    s3.download_object(obj, filename=Path("data"))
+    s3.download_object(obj, filename=data_dir)
